1fb4a2c15b40820e60180a2ad89e0e5c818cc779
HugrunH <hugrun@art.is>
2018-12-02 12:40:27 +0000

    Implemented code for foot idle to walking animation + animation for both players if they change directions

---
+++ Experiment_804/Assets/PlayerFootMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootMovement : MonoBehaviour
+{
+
+    //Script in use
+    private Animator animator;
+    //Speed of the player
+    public float speed;
+    // Use this for initialization
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Player_Foot_Horizontal");
+        //Checking which direction the foot turns to
+        if (horizontal > 0 && transform.localScale.x < 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (horizontal < 0 && transform.localScale.x > 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        //Checking if the Foot is Walking
+        if ((Input.GetKey("left") || Input.GetKey("right"))) {
+            float footPosition = transform.position.x + horizontal * speed * Time.deltaTime;
+            transform.position = new Vector2(Mathf.Clamp(footPosition, -8f, 8f), 0.3f);
+        }
+        //Set the float footWalking to the horizontal value
+        animator.SetFloat("FootWalking", Mathf.Abs(horizontal));
+    }
+}
--- Experiment_804/Assets/Scripts/PlayerMovement.cs
+++ Experiment_804/Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,25 @@ public class PlayerMovement : MonoBehaviour {
     //Script in use
     private Animator animator;
 
-    private Rigidbody2D rb2D;
     //Speed of the player
     public float speed;
 	// Use this for initialization
 	void Awake () {
-        rb2D = GetComponent<Rigidbody2D> ();
         animator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontal = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxis("Player_Hand_Horizontal");
+        //Checking which direction the hand turns to
+        if (horizontal > 0 && transform.localScale.x < 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (horizontal < 0 && transform.localScale.x > 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
         //Checking if the Hand is Walking
         if (Input.GetKey("a") || Input.GetKey("d")) {
             float handPosition = transform.position.x + horizontal * speed * Time.deltaTime;
