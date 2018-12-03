a5531ccfd99dadbd9c783a26aeb9c4897034f9f9
obelix09 <petra091@gmail.com>
2018-12-03 18:26:15 +0000

    players fade through the door, ready for next level

--- Experiment_804/Assets/Scripts/NextLevel.cs
+++ Experiment_804/Assets/Scripts/NextLevel.cs
@@ -10,13 +10,30 @@ public class NextLevel : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player_Hand")
-        {
+        { 
+            var hand = FindObjectOfType<PlayerHandMovement>();
+            hand.enabled = false;
+            var handAnimator = hand.GetComponent<Animator>();
+            handAnimator.SetBool("Jumping", false);
+            handAnimator.SetFloat("HandWalking", 0);
+            handAnimator.Play("Hand_Faded");
+            Destroy(hand.gameObject, 1f);
             handInDoor = true;
         }
+
         if (col.gameObject.tag == "Player_Foot")
         {
+            var foot = FindObjectOfType<PlayerFootMovement>();
+            foot.enabled = false;
+            foot.GetComponent<Animator>().Play("Foot_Faded");
+            var footAnimator = foot.GetComponent<Animator>();
+            footAnimator.SetBool("Jumping", false);
+            footAnimator.SetFloat("HandWalking", 0);
+            footAnimator.Play("Hand_Faded");
+            Destroy(foot.gameObject, 1f);
             footInDoor = true;
         }
+
         if (footInDoor == true && handInDoor == true)
         {
             Debug.Log("YAY YOU GET TO GO TO THE NEXT LEVEL!!!");
