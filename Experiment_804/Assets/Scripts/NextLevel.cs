58395edb88e380665b7865e4d3903d4cc84e4151
unknown <hjorturjohann94@gmail.com>
2018-12-03 19:00:30 +0000

    Reformatted code again so Hugrun wont die from an heart attack

--- Experiment_804/Assets/Scripts/NextLevel.cs
+++ Experiment_804/Assets/Scripts/NextLevel.cs
@@ -2,35 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NextLevel : MonoBehaviour
-{
+public class NextLevel : MonoBehaviour {
     private bool handInDoor;
     private bool footInDoor;
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "Player_Hand")
-        {
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player_Hand") {
             handInDoor = true;
         }
-        if (col.gameObject.tag == "Player_Foot")
-        {
+        if (col.gameObject.tag == "Player_Foot") {
             footInDoor = true;
         }
-        if (footInDoor == true && handInDoor == true)
-        {
+        if (footInDoor == true && handInDoor == true) {
             Debug.Log("YAY YOU GET TO GO TO THE NEXT LEVEL!!!");
         }
     }
 
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "Player_Hand")
-        {
+    private void OnTriggerExit2D(Collider2D col) {
+        if (col.gameObject.tag == "Player_Hand") {
             handInDoor = false;
         }
-        if (col.gameObject.tag == "Player_Foot")
-        {
+        if (col.gameObject.tag == "Player_Foot") {
             footInDoor = false;
         }
     }
