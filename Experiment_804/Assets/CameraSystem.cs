0a2063de561d965779fb9769139aa4a8c7ef2655
unknown <hjorturjohann94@gmail.com>
2018-12-04 16:03:50 +0000

    Added a basic camera system

---
+++ Experiment_804/Assets/CameraSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour {
+
+    public float smoothing = 0.5f;
+
+    public GameObject Hand;
+    public GameObject Foot;
+
+    public float followPositionX;
+    public float followPositionY;
+
+    private Vector3 followPosition;
+
+    private void Awake() {
+        followPositionX = (Hand.transform.position.x - Foot.transform.position.x) * 0.5f;
+        followPositionY = transform.position.y;
+
+        followPosition = new Vector3(followPositionX, followPositionY, transform.position.z);
+    }
+
+    private void LateUpdate() {
+        followPosition.x = (Hand.transform.position.x + Foot.transform.position.x) * 0.5f;
+
+        Vector3 newPosition = Vector3.Lerp(transform.position, followPosition, smoothing);
+        transform.position = newPosition;
+    }
+}
