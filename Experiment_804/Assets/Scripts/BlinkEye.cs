c30bfc977a9c329ba4fea75c170041cd0b0b1cb4
obelix09 <petra091@gmail.com>
2018-12-05 15:07:28 +0000

    level 1 background and nd animation added

---
+++ Experiment_804/Assets/Scripts/BlinkEye.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkEye : MonoBehaviour {
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        StartCoroutine(BlinkDelay());
+    }
+
+    private IEnumerator BlinkDelay()
+    {
+        while (true)
+        {
+            animator.Play("Blink_Eye", 0, -1);
+            yield return new WaitForSeconds(8f);
+        }
+    }
+}
