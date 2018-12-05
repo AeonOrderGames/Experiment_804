45dccd4e78f9ea310a8702133534dad7554a9aaf
HugrunH <hugrun@art.is>
2018-12-05 15:03:13 +0000

    Experiment with the camera for coop, not fixed

--- Experiment_804/Assets/Scripts/CameraSystem.cs
+++ Experiment_804/Assets/Scripts/CameraSystem.cs
@@ -11,20 +11,20 @@ public class CameraSystem : MonoBehaviour {
 
     public float followPositionX;
     public float followPositionY;
-
     private Vector3 followPosition;
+    private Vector3 newPosition;
 
     private void Awake() {
-        followPositionX = (Hand.transform.position.x - Foot.transform.position.x) * 0.5f;
+        followPositionX = (Hand.transform.position.x + Foot.transform.position.x) * 0.5f;
         followPositionY = transform.position.y;
 
         followPosition = new Vector3(followPositionX, followPositionY, transform.position.z);
     }
 
     private void LateUpdate() {
-        followPosition.x = (Hand.transform.position.x + Foot.transform.position.x) * 0.5f;
+            followPosition.x = (Hand.transform.position.x + Foot.transform.position.x) * 0.5f;
 
-        Vector3 newPosition = Vector3.Lerp(transform.position, followPosition, smoothing);
-        transform.position = newPosition;
+            newPosition = Vector3.Lerp(transform.position, followPosition, smoothing);
+            transform.position = newPosition;
     }
 }
