bffe3c0c329384f7f86eae61beb6b5c0ee148e59
HugrunH <hugrun@art.is>
2018-12-05 17:48:35 +0000

    Implemented fade black between scenes + main scene goes to leveone when they finished it

--- Experiment_804/Assets/Scripts/NextLevel.cs
+++ Experiment_804/Assets/Scripts/NextLevel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour {
     private bool handInDoor;
@@ -33,7 +34,7 @@ public class NextLevel : MonoBehaviour {
         }
 
         if (footInDoor == true && handInDoor == true) {
-
+            Initiate.Fade("Level_One", Color.black, 2f);
         }
     }
 
