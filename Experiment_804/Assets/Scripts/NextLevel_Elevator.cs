ef41d88282abeea33e2b2028a0f578187412a7ad
HugrunH <hugrun@art.is>
2018-12-09 19:04:52 +0000

    Connected the elevator to level_1 and level_2

---
+++ Experiment_804/Assets/Scripts/NextLevel_Elevator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel_Elevator : MonoBehaviour {
+
+    public int nextScene;
+    private string[] sceneNames;
+
+    public void Awake() {
+        nextScene = SceneManager.GetActiveScene().buildIndex;
+        sceneNames = new string[] { "Main", "Level_One", "Elevator_One", "Level_Two" };
+        StartCoroutine(SwitchScenes());
+    }
+
+    private IEnumerator SwitchScenes() {
+        yield return new WaitForSeconds(11f);
+        Initiate.Fade(sceneNames[nextScene], Color.black, 1f);
+    }
+}
