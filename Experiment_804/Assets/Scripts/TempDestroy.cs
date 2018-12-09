4b1a9f5588e87c0a90221f1c9f3528d20d64847e
HugrunH <hugrun@art.is>
2018-12-09 18:48:10 +0000

    Implemented a dialog when the foot grabs the leg

--- Experiment_804/Assets/Scripts/TempDestroy.cs
+++ Experiment_804/Assets/Scripts/TempDestroy.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class TempDestroy : MonoBehaviour {
 
+    public Canvas dialogCanvas;
+    public Level_1_Dialog dialogue;
+
     private void OnTriggerEnter2D(Collider2D col) {
-        if(col.gameObject.CompareTag("Player_Foot"))
+        if (col.gameObject.CompareTag("Player_Foot")) {
             Destroy(gameObject);
+            //Freezing the screen
+            Time.timeScale = 0.0f;
+            dialogCanvas.gameObject.SetActive(true);
+            TriggerDialogue();
+        }
+    }
+
+    public void TriggerDialogue() {
+        FindObjectOfType<Level_1_DialogManager>().StartDialogue(dialogue);
     }
 }
