3d0ce8595c451caa0ab121cb34eb45655d9a4b2f
obelix09 <petra091@gmail.com>
2018-12-09 18:52:16 +0000

    elevator complete

---
+++ Experiment_804/Assets/ElevatorDing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDing : MonoBehaviour {
+    private AudioSource sound;
+
+    // Use this for initialization
+    void Start () {
+        sound = GetComponent<AudioSource>();
+        StartCoroutine(Ding());
+    }
+
+    private IEnumerator Ding()
+    {
+        yield return new WaitForSeconds(10.5f);
+        sound.Play();
+    }
+}
