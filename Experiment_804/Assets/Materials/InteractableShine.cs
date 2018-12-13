5d7082a83e3a61da2aa25f1cc49a072bd7f79438
unknown <hjorturjohann94@gmail.com>
2018-12-13 14:44:13 +0000

    Added interactable shader and shine to box and vent in level one

---
+++ Experiment_804/Assets/Materials/InteractableShine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableShine : MonoBehaviour {
+
+    private Material shine;
+    private float val;
+    public float shaderWidth = 0.01f;
+    public float cooldown = 2f;
+
+	// Use this for initialization
+	void Start () {
+        shine = GetComponent<SpriteRenderer>().material;
+        shine.SetFloat("_ShineWidth", shaderWidth);
+        StartCoroutine(ShinePeriodically());
+	}
+	
+	private IEnumerator ShinePeriodically() {
+
+        while (true) {
+            val = 0f;
+            yield return new WaitForSeconds(cooldown);
+
+            while (val < 1f) {
+                shine.SetFloat("_ShineLocation", val);
+                yield return null;
+                val += 0.01f;
+            }
+        }
+    }
+}
