d0ae27369ebc8aad1bbea6993aff486ddb1baf65
HugrunH <hugrun@art.is>
2018-12-13 16:32:01 +0000

    Implemented level reload if players die in level_3 + fixed level_2 reload

---
+++ Experiment_804/Assets/AcidPool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidPool : MonoBehaviour {
+    private bool armDead = false;
+    private bool legDead = false;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.name == "Player_Leg") {
+            col.gameObject.GetComponent<Animator>().Play("Leg_Death");
+            legDead = true;
+        }
+        if (col.gameObject.name == "Player_Arm") {
+            col.gameObject.GetComponent<Animator>().Play("Arm_Death");
+            armDead = true;
+        }
+        if (armDead || legDead) {
+            StartCoroutine(fadeTimer());
+        }
+    }
+
+    private IEnumerator fadeTimer() {
+        yield return new WaitForSeconds(1f);
+        Initiate.Fade("Level_Three", Color.black, 2f);
+    }
+
+
+
+}
