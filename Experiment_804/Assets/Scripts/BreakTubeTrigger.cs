f6c97e0628fe1d1cd5784ff34b125d6bf6c44c5b
unknown <hjorturjohann94@gmail.com>
2018-12-14 11:23:58 +0000

    Shine changes to level 2

--- Experiment_804/Assets/Scripts/BreakTubeTrigger.cs
+++ Experiment_804/Assets/Scripts/BreakTubeTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BreakTubeTrigger : MonoBehaviour {
 
     public GameObject brokenTube;
-    public LegMovement leg;
+    private LegMovement leg;
 
 	// Use this for initialization
 	void Start () {
